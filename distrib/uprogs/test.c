1713ad774030465464deea312cfa3bab03771cba
chehang <thss15_cheh@163.com>
2017-06-14 13:29:25 +0800

    Add simple printf

--- distrib/uprogs/test.c
+++ distrib/uprogs/test.c
@@ -5,6 +5,9 @@ volatile int count = 0;
 
 int main(int argc, char *argv[])
 {
-    printf(1, "hello, world!\n");
+    long i = 1023;
+    const char *p = "world";
+    double pi = 3.14159;
+    myprintf("hello, %10s: %#06x   %.5f!\n", p, i, pi / 0);
     exit();
 }
--- include/xv6/user.h
+++ include/xv6/user.h
@@ -33,7 +33,7 @@ void* malloc(uint);
 void free(void*);
 int atoi(const char*);
 
-
+// string.h
 uint strlen(const char *str);
 char * strchr(register const char *p, register const char ch);
 char *strrchr(register const char *p, register const char *ch);
@@ -48,3 +48,27 @@ void *memcpy(void *dst0, const void *src0, register uint length);
 void *memccpy(void *t, const void *f, int c, register uint n);
 void *memchr(const void *s, register unsigned char c, register uint n);
 int memcmp(const void *s1, const void *s2, uint n);
+
+#define NULL (0)
+#define EOF  (-1)
+
+// stdio.h
+typedef struct {
+    int fd;
+} FILE;
+
+extern FILE _std_files[];
+
+#define stdin (&_std_files[0])
+#define stdout (&_std_files[1])
+
+extern int myprintf(char const *fmt, ...);
+
+// stdarg.h
+typedef char *va_list;
+#define	va_arg(ap, type) ((type *)(ap += sizeof(type)))[-1]
+#define	va_end(ap)
+#define	__va_promote(type) (((sizeof(type) + sizeof(int) - 1) / sizeof(int)) * sizeof(int))
+#define	va_start(ap, last) (ap = ((char *)&(last) + __va_promote(last)))
+
+#define	HUGE_VAL	1.701411834604692294E+38
