4b8e8a6047179e78d37fc4df40d6b74e61cf20ff
jjr15 <1506847103@qq.com>
2017-06-19 21:49:51 +0800

    6/19 21.43

--- kernel/proc_init.c
+++ kernel/proc_init.c
@@ -2,34 +2,74 @@
 #include "xv6/defs.h"
 #include "xv6/param.h"
 #include "xv6/mmu.h"
+#include "xv6/spinlock.h"
 #include "xv6/proc.h"
 #include "xv6/proc_fs.h"
 
 void  proc_root_init(void)
 {
-  procfs = 0;
   root=pde_alloc(PDE_DIR);
   safestrcpy(root->name,"proc",5);
   root->namelen=4;
   root->type=PDE_DIR;
   root->data=0;
   root->read_proc=read_dir_list;
-  //newpde.write_proc=write_proc;
   //insert
   root->subdir=0;
   root->parent=root;
   root->next=0;
+  root->pre=0;
+<<<<<<< HEAD
+  allocproci(root);
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+  allocproci(root);
+=======
+>>>>>>> 079dade4e68029419f9b4b4559287e562e7ad59b
+>>>>>>> 6d0e71a717f320a2b92b6f993bce178c53490d98
+=======
+>>>>>>> 079dade4e68029419f9b4b4559287e562e7ad59b
+>>>>>>> 6d0e71a717f320a2b92b6f993bce178c53490d98
+=======
+>>>>>>> 079dade4e68029419f9b4b4559287e562e7ad59b
+>>>>>>> 6d0e71a717f320a2b92b6f993bce178c53490d98
+=======
+>>>>>>> 079dade4e68029419f9b4b4559287e562e7ad59b
+>>>>>>> 6d0e71a717f320a2b92b6f993bce178c53490d98
 }
 void  proc_cpuinfo_init(void)
 {
   proc_mkdir("cpuinfo",PDE_FILE,root, 0, read_cpuinfo);
 }
-void  proc_process_init(struct proc*process)
+void  proc_add_process(struct proc*process)
 {
   char numstr[10];
   num_to_str(numstr,process->pid,0);
   struct proc_dir_entry*proc_dir=proc_mkdir(numstr,PDE_DIR,root,process,read_dir_list);
   proc_mkdir("status",PDE_FILE,proc_dir,process,read_proc_stat);
+}
+
+void proc_del_process(unsigned int pid)
+{
+  int i,n;
+  struct proc_dir_entry*p=root->subdir;
+  while(p!=0)
+  {
+    if(p->type==PDE_DIR)
+    {
+      i=0;n=0;
+      while(i<p->namelen)
+        n=n*10+p->name[i];
+      if(n==pid)
+      {
+        remove_proc_entry_recursive(p);
+        break;
+      }
+    }
+  }
 }
 
 void proc_update()
@@ -39,6 +79,7 @@ void proc_update()
 
 void  proc_init(void)
 {
+  //cprintf("procinit\n");
   proc_root_init();
   proc_cpuinfo_init();
   //proc_update()；
